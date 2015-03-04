21e2da6c481cf21311e0bd2e95c9bf7f89f1a77d
agentbillo <billo@billo.com>
2015-03-04 16:32:11 -0500

    checkpoint

--- gobloggo.go
+++ gobloggo.go
@@ -1,8 +1,11 @@
 package main
-
+// to compile and run:
+// go install github.com/agentbillo/gobloggo
+// ~/go/bin/gobloggo
 import "fmt"
 import "os"
 import "strings"
+import "sort"
 
 var forcemarkdown bool = false;
 var blogdir string = "/users/billo/sites/egopoly.com/blog"
@@ -29,7 +32,7 @@ func options() {
     }
 }
 
-func blogscan (path string) {
+func listdir (path string) []string {
     dir, err := os.Open(path)
     if err != nil {
         fmt.Println(err)
@@ -41,8 +44,61 @@ func blogscan (path string) {
         fmt.Println(err)
         os.Exit(1)
     }
+    entries := make([]string, 0)
     for _,f := range files {
-        fmt.Println(f.Name())
+        name := f.Name()
+        entries = append(entries, name)
+    }
+    sort.Strings(entries)
+    return entries
+}
+
+func monthscan (path string, month string) {
+    fmt.Println(month)
+    monthdir := path + "/" + month
+    entries := listdir(monthdir)
+    textfiles := make([]string, 0)
+    for _,e := range entries {
+        if strings.HasSuffix(e, ".txt") {
+            textfiles = append(textfiles, e)
+        }
+    }
+    sort.Strings(textfiles)
+    fmt.Println(textfiles)
+}
+
+
+func yearscan (path string, year string) {
+    fmt.Println(year)
+    yeardir := path + "/" + year
+    entries := listdir(yeardir)
+    months := make([]string, 0)
+    for _,e := range entries {
+        if len(e) == 2 {
+            months = append(months, e)
+        }
+    }
+    sort.Strings(months)
+    for _,month := range months {
+        monthscan(yeardir, month)
+    }
+}
+
+
+func blogscan (path string) {
+
+    entries := listdir(path)
+    years := make([]string, 0)
+    for _,e := range entries {
+        // go blog go only worjs in the 21 century
+        if strings.HasPrefix(e, "20") && len(e) == 4 {
+            years = append(years, e)
+        }
+    }
+    sort.Strings(years)
+    fmt.Println(years)
+    for _,year := range years {
+        yearscan(path, year)
     }
 }
 
