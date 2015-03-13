8aa994a0afe393eed6c6926adea81beb89c9766d
agentbillo <billo@billo.com>
2015-03-13 10:51:53 -0400

    actually writing files with Markdown now

--- gobloggo.go
+++ gobloggo.go
@@ -2,12 +2,20 @@ package main
 // to compile and run:
 // go install github.com/agentbillo/gobloggo
 // ~/go/bin/gobloggo
+//
+// to run test:
+// gobloggo -blog ~/Sites/blog -master ~/Sites/blog
+//
 import "fmt"
 import "os"
+import "os/exec"
 import "strings"
 import "sort"
 import "regexp"
 import "bufio"
+import "io/ioutil"
+
+//import "time"
 
 var forcemarkdown bool = false;
 var blogdir string = "/users/billo/sites/egopoly.com/blog"
@@ -30,6 +38,7 @@ type blogpost struct {
     month string
     shtmlfile string
     preview string
+    stamp string
 }
 
 var monthmap = make(map[string]*blogmonth)
@@ -54,6 +63,32 @@ func options() {
     }
 }
 
+// returns true if path1 ctime is older than path2 ctime
+// if either path is unstat-able then return false
+func isolder(path1 string, path2 string) bool {
+    info1,err := os.Stat(path1)
+    if err == nil {
+        info2,err := os.Stat(path2)
+        if err == nil {
+            //fmt.Printf("time comparison %s %s %s %s\n", path1, info1.ModTime(), path2, info2.ModTime())
+            if info1.ModTime().Before(info2.ModTime()) {
+                return true
+            }
+            return false
+        }
+    }
+    return false
+}
+
+func pathexists(path string) bool {
+    if _, err := os.Stat(path); err != nil {
+        if os.IsNotExist(err) {
+            return false
+        }
+    }
+    return true
+}
+
 func postprocess (monthdir string, year string, month string, postfile string) {
     var thismonth *blogmonth
     thismonth = monthmap[monthdir]
@@ -66,7 +101,7 @@ func postprocess (monthdir string, year string, month string, postfile string) {
     slug := ""
     if len(matches) > 1 {
         slug = matches[1]
-        fmt.Printf("slug is %s\n", slug)
+        //fmt.Printf("slug is %s\n", slug)
     }
     title := "wut"
     datestamp := "1970-01-01 00:00"
@@ -91,24 +126,36 @@ func postprocess (monthdir string, year string, month string, postfile string) {
             lc++
         }
         fmt.Printf("title = %s\n", title)
-        fmt.Printf("stamp = %s\n", datestamp)
-        fmt.Println(preview)
+        //fmt.Printf("stamp = %s\n", datestamp)
+        //fmt.Println(preview)
 
         filepath := fmt.Sprintf("%s/%s.txt", monthdir, slug)
         htmlpath := fmt.Sprintf("%s/%s.ihtml", monthdir, slug)
-        shtmlpath := fmt.Sprintf("%s/%s.shtml", monthdir, slug)
+        //shtmlpath := fmt.Sprintf("%s/%s.shtml", monthdir, slug)
         shtmlbasepath := fmt.Sprintf("%s.shtml", slug)
         plainhtmlbasepath := fmt.Sprintf("%s.html", slug)
         url := fmt.Sprintf("%s/%s/%s.html", year, month, slug)
 
         post := &blogpost{title, url, plainhtmlbasepath, monthdir, postfile, slug, year,
-            month, shtmlbasepath, preview}
+            month, shtmlbasepath, preview, datestamp}
 
-        mdcmd := fmt.Sprintf("Markdown.pl --html4tags %s > %s", filepath, htmlpath)
+        mdcmd := fmt.Sprintf("Markdown.pl --html4tags %s --output %s", filepath, htmlpath)
+        if forcemarkdown || !pathexists(htmlpath) || isolder(htmlpath, filepath) {
+            cmd := exec.Command("Markdown.pl", "--html4tags", filepath)
+            output, err := cmd.Output()
+            if err != nil {
+                fmt.Printf("***Markdown failed: %s\n", err)
+                return
+            }
+            err = ioutil.WriteFile(htmlpath, output, 0644)
+            if err != nil {
+                panic(err)
+            }
+            fmt.Printf("%s\n", post.title)
+            //fmt.Printf("%s\n", shtmlpath)
+            fmt.Printf("%s\n", mdcmd)
+        }
 
-        fmt.Printf("%s\n", post.title)
-        fmt.Printf("%s\n", shtmlpath)
-        fmt.Printf("%s\n", mdcmd)
         
 
 
